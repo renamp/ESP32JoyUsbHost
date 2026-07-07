f6dae7d449c1a264bfce2ff7d65b59800c7886cd
Renan <renamp2006@gmail.com>
2026-07-07 09:24:56 +0000

    Add CRC-16
     - Implement CRC-16 for data packet.
     - Add file to CMakeLists.txt

---
+++ main/inc/main.h
@@ -0,0 +1,18 @@
+/**
+  ******************************************************************************
+  * @file      main.h  [7/7/2026]
+  * @author    Renan A. Pacheco
+  * @brief   Main header
+  ******************************************************************************
+  */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+
+
+#endif /* MAIN_H */
