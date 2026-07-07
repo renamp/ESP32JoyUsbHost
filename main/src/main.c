2cfeb328f38e84569f30d9da0c6a347cf60e266e
Renan <renamp2006@gmail.com>
2026-07-07 08:25:55 +0000

    Initial Project structure
    - Added main.c file
    - Added CMakeLists files to build project

---
+++ main/src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void taskUsb(void *pvParameters);
+
+void app_main(void)
+{
+    xTaskCreate(taskUsb, "taskUsb", 1024, NULL, 1, NULL);
+}
+
+
+void taskUsb(void *pvParameters)
+{
+    printf("Task USB is running...\n");
+    while(1) {
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+}
